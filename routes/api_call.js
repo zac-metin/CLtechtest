f91861ed953068934a333f4eedb2400e6bd96001
zac-metin <zac.metin@gmail.com>
2017-09-25 00:51:51 +1300

    routes built

--- routes/api_call.js
+++ routes/api_call.js
@@ -82,7 +82,70 @@ router.get('/query/:queryStatement', function (req, res) {
 
   var preEncodeUrl = config.api_uri + req.session.realmId + '/query?query=' + req.params.queryStatement
   var url = encodeURI(preEncodeUrl)
-  console.log('Making API call to: ' + url)
+  console.log('Making API query of '+ req.params.queryStatement + ' to ' + url)
+  var requestObj = {
+    url: url,
+    headers: {
+      'Authorization': 'Bearer ' + token.accessToken,
+      'Accept': 'application/json'
+    }
+  }
+
+  request(requestObj, function (err, response) {
+    tools.checkForUnauthorized(req, requestObj, err, response).then(function ({err, response}) {
+      if(err || response.statusCode != 200) {
+        return res.json({error: err, statusCode: response.statusCode})
+      }
+      res.json(JSON.parse(response.body))
+    }, function (err) {
+      console.log(err)
+      return res.json(err)
+    })
+  })
+})
+
+router.post('/create/:createName', function (req, res) {
+  var token = tools.getToken(req.session)
+  if(!token) return res.json({error: 'Not authorized'})
+  if(!req.session.realmId) return res.json({
+    error: 'No realm ID.  QBO calls only work if the accounting scope was passed!'
+  })
+
+  var url = config.api_uri + req.session.realmId + '/' + req.params.createName
+  console.log('Making API create / update call to: ' + url)
+  console.log('Sending the following infoo: ' + req.body)
+  var requestObj = {
+    url: url,
+    headers: {
+      'Authorization': 'Bearer ' + token.accessToken,
+      'Accept': 'application/json'
+    },
+    body: req.body
+  }
+
+  request(requestObj, function (err, response) {
+    tools.checkForUnauthorized(req, requestObj, err, response).then(function ({err, response}) {
+      if(err || response.statusCode != 200) {
+        return res.json({error: err, statusCode: response.statusCode})
+      }
+
+      res.json(JSON.parse(response.body))
+    }, function (err) {
+      console.log(err)
+      return res.json(err)
+    })
+  })
+})
+
+router.post('/delete/:createName', function (req, res) {
+  var token = tools.getToken(req.session)
+  if(!token) return res.json({error: 'Not authorized'})
+  if(!req.session.realmId) return res.json({
+    error: 'No realm ID.  QBO calls only work if the accounting scope was passed!'
+  })
+
+  var url = config.api_uri + req.session.realmId + '/' + req.params.resourceName + "?operation=delete"
+  console.log('Making API deletion call to: ' + url)
   var requestObj = {
     url: url,
     headers: {
@@ -104,5 +167,6 @@ router.get('/query/:queryStatement', function (req, res) {
     })
   })
 })
+
 
 module.exports = router
